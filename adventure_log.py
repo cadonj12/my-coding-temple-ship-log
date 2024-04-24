9b1d44046cc9d9d89a788977211a497487b38827
cadonj12 <cadonjohnson21@gmail.com>
2024-04-23 17:56:26 -0700

    updated python file

--- adventure_log.py
+++ adventure_log.py
@@ -1,5 +1,5 @@
 def greet_coding_temple():
-    print("Hello, Coding Temple Navigators!")
+    print("Hello, Coding Temple Navigators!!")
           
 if __name__ == "_main_":
     greet_coding_temple()
